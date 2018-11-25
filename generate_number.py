9d6b6341d7add68df9fc2119b874584cad13d022
Dmitriy <45180222+iAnafem@users.noreply.github.com>
2018-11-25 23:17:12 +0300

    Add files via upload

---
+++ generate_number.py
@@ -0,0 +1,15 @@
+def generate_number(N:int, M:int, prefix=None):
+    """ Генерирует все числа (с лидирующими незначащими нулями)
+        в N-ричной системе счисления (N <= 10)
+        длины М
+    """
+    prefix = prefix or []
+    if M == 0:
+        print(prefix)
+        return
+    for digit in range(N):
+        prefix.append(digit)
+        generate_number(N, M-1, prefix)
+        prefix.pop()
+
+generate_number(4, 4)
